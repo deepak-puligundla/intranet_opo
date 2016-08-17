aa67f142e4d8895c3f7b7c15450082a4b63993c8
deepak <deepak@oneplanetops.com>
2016-08-17 15:10:11 -0700

    Add managementTeam page

---
+++ src/managementTeam.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+  <meta name="generator" content="Bluefish 2.2.7" >
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content=
+  "width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Deepak p" >
+
+  <title>One Planet Ops - Intranet</title>
+  <!-- Bootstrap Core CSS -->
+  <link href="/OPOIntranet/css/bootstrap.css" rel="stylesheet"
+  type="text/css">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+  <div class="container">
+    <?php include_once("navigationHeader.html");?>
+  </div>
+  
+  <!-- container one -->
+
+  <div class="container">
+  
+  
+  </div>
+</body>
+</html>
