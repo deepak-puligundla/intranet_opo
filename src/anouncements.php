4675826c5f4bc492bca071919a72cbccac6704bb
deepak-puligundla <deepakp@opo.local>
2016-11-28 15:25:02 -0800

    Add handbook and anouncements pages

--- src/anouncements.php
+++ src/anouncements.php
@@ -25,15 +25,7 @@
       <div class="col-sm-8">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/4NCzwSZ8CPg" frameborder="0" allowfullscreen></iframe>
       </div>
-      <div class="col-sm-4">
-
-      </div>
     </div>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="http://intranet.oneplanetops.com/intranet_opo/css/bootstrap-3.3.7-dist/js/bootstrap.js"></script>
 
 </body>
 </html>
---
+++ src/eHandbook.php
@@ -0,0 +1,110 @@
+<?php $pageId="eHandbook"; ?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Deepak Puligundla" >
+
+  <title>One Planet Ops - Intranet</title>
+  <!-- Bootstrap Core CSS -->
+  <link href="http://intranet.oneplanetops.com/intranet_opo/css/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <?php include_once("navigationHeader.html");?>
+
+    <div class="container">
+      <div class="page-header">
+        <h2>Employee Handbook</h2>
+      </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading"><b><h4>MISSION STATEMENT</h4></b></div>
+      <div class="panel-body">
+      We exist to deliver innovations that enable scalable & efficient local marketing.
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading"><b><h4>CORE VALUES</h4></b></div>
+      <div class="panel-body">
+        <b>Integrity guides all our actions</b>
+        <ul>
+          <li>We conduct all business with uncompromising integrity</li>
+          <li>We honor our professional commitments</li>
+          <li>We take personal responsibility for managing results and meeting goals</li>
+        </ul>
+
+        <b>Customers are our first concern</b>
+        <ul>
+          <li>We exist to serve the needs of our customers</li>
+          <li>Our clients are the lifeblood of our company</li>
+          <li>We strive to be the preferred choice of our customers and clients</li>
+          <li>We focus on the development and retention of customer and client relationships</li>
+        </ul>
+
+        <b>Business Excellence is our goal</b>
+        <ul>
+          <li>We demonstrate competitively superior quality in all our products and services</li>
+          <li>We are obsessed with managing costs and eliminating waste</li>
+          <li>We recognize value creation is best measured by sustained profitability</li>
+          <li>We drive innovation through intelligent risk-taking to forge a better company</li>
+        </ul>
+
+        <b>Our people are our foundation</b>
+        <ul>
+          <li>We value our employees’ contributions to the growth and profitability of One Planet Ops</li>
+          <li>We are committed to our team members’ professional development and personal success</li>
+          <li>One Planet Ops cultivates a fun, rewarding, and enjoyable experience for our customers, clients and employees</li>
+        </ul>
+
+      </div>
+    </div>
+
+    <pre>
+      Dear Co-Worker:
+
+      Welcome to One Planet Ops
+
+      We are very excited to have you as a member of our progressive and dynamic team.  You were hired because we believe you have the talent and ability necessary to make a
+      significant contribution to the growth of our company and to play an integral role in our success.  Most importantly, as a fellow employee and an associate we believe you
+      share our commitment to our mission statement and the corporate values that guide our fulfillment of that mission.
+
+      I hope this will be a rewarding and fulfilling experience for you.  As part of the One Planet Ops team you will find that the pursuit of excellence while undertaking and
+      achieving challenging business objectives is a significant part of our corporate culture.  Each of us takes personal responsibility for managing results and meeting both our
+      individual and corporate goals.
+
+      To achieve success we need to bring our individual effort and creativity together and be able to perform as a unified team.  Communication and a willingness to appreciate
+      the experience and perspective of others is an important part of teamwork.  We believe that “none of us is as smart as all of us,” and that together we can accomplish great
+      things as an organization.  We must also always remember that what we sell is service.  Whatever role you may play here, whether you are in direct contact with consumers
+      or clients or not, you deliver that service!
+
+      Each and every one of our jobs at One Planet Ops is critically important to the success of the company in achieving its mission.  And our primary goals are to embody our
+      mission statement, adhere to our corporate values and contribute to securing our place as the leader in our industry.
+
+      You can be assured that it is the strong desire of every member of our management team that you succeed in your job and realize your full potential as a professional.
+
+      We will all work together to make One Planet Ops one of the nation’s best places to work as decided by those who would know best – this company’s employees.
+
+      You should use this handbook as a ready reference as you pursue your career with One Planet Ops.  Additionally, the handbook should assure good management and fair
+      treatment of all employees.  Welcome aboard.  We look forward to your contribution.
+
+      <b>
+      Sincerely,
+
+      Payam Zamani
+
+      President & CEO </b>
+    </pre>
+
+    </div>
+</body>
+</html>
